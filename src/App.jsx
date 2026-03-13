10a84879779e4ca851592cc8c1534988fc6a765f
Aishahaha <76033573+Aishahaha@users.noreply.github.com>
2026-03-13 23:55:08 +0800

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold leading-tight tracking-tighter sm:text-3xl md:text-4xl lg:leading-[1.1]">
+        Aishah Dawood
+      </h1>
+      <h1 class="text-2xl font-bold leading-tight tracking-tighter sm:text-3xl md:text-4xl lg:leading-[1.1] mt-2 text-muted-foreground">
+        A coder by day, problem-solver by night!
+      </h1>
+      <p class="w-full text-left text-base font-light text-foreground sm:text-lg">
+        I am a dedicated Software Engineer specializing in full-stack
+        application development. I enjoy crafting responsive web solutions using
+        modern technologies like Next.js, React, and Tailwind CSS. Currently, I
+        am expanding my skills into mobile development with React Native and
+        Expo, aiming to deliver comprehensive, user-centric software solutions.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
